a33c7c4f163f2b134da28868d0cde513241e8aa9
Melissa Lewis <meli.lewis@gmail.com>
2013-09-02 18:34:26 -0400

    disqus again

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -5,7 +5,7 @@ from __future__ import unicode_literals
 AUTHOR = u'Melissa'
 SITENAME = u'OR to VA'
 SITEURL = ''
-THEME = "/Users/m/pelican-themes/cebong"
+THEME = "/Users/m/pelican-themes/elegant"
 
 TIMEZONE = 'America/New_York'
 
@@ -19,6 +19,9 @@ ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+
+#Disqus
+DISQUS_SITENAME = 'meli-lewis'
 
 # Blogroll
 LINKS =  (('Pelican', 'http://getpelican.com/'),)
@@ -34,3 +37,12 @@ RELATIVE_URLS = True
 
 # Automatically generate date
 DEFAULT_DATE = 'fs'
+
+#elegant theme-related variables
+#PLUGINS = ['sitemap', 'extract_toc', 'tipue_search']
+#MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
+#DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
+#STATIC_PATHS = ['theme/images', 'images']
+#TAG_SAVE_AS = ''
+#CATEGORY_SAVE_AS = ''
+#AUTHOR_SAVE_AS = ''

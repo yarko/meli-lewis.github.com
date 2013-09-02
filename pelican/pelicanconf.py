1cea1aef485731707997efde6a992dc760bf91df
Melissa Lewis <meli.lewis@gmail.com>
2013-09-02 16:03:44 -0400

    modifying article URL

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -11,20 +11,21 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
+# Set the article URL
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('Pelican', 'http://getpelican.com/'),)
 
 # Social widget
 SOCIAL = (('Twitter', 'https://twitter.com/meli_lew'),
-          ('Another social link', '#'),)
+          ('GitHub', 'https://github.com/meli-lewis/'),)
 
 DEFAULT_PAGINATION = 10
 
